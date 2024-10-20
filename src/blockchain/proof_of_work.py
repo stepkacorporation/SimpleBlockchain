31b0bc3165b70935c02ab63d04790f0ed0265636
Stepan <kornevstepan169@gmail.com>
2024-10-20 22:57:42 +0700

    minor fixes

--- src/blockchain/proof_of_work.py
+++ src/blockchain/proof_of_work.py
@@ -20,7 +20,7 @@ class ProofOfWork:
             nonce += 1
 
     def is_valid_nonce(self, block: Block) -> bool:
-        """Check if a block's hash  is valid based on the PoW difficulty"""
+        """Check if a block's hash is valid based on the PoW difficulty"""
 
         prefix_str = '0' * self.difficulty
         return block.compute_hash().startswith(prefix_str)
--- src/config.py
+++ src/config.py
@@ -2,7 +2,3 @@ import uuid
 from src.blockchain import Blockchain
 
 blockchain = Blockchain()
-
-# ID of the current node
-node_id = str(uuid.uuid4()).replace('-', '')
-
